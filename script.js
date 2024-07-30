dcd137705d2bef9eb87fdc80130480ff25b8b856
itteamlxs <itbkup24@gmail.com>
2024-07-30 10:46:03 -0600

    Set up project with basic functionality
    - Added HTML structure for document display
    - Implemented CSS for responsive design and styling
    - Added JavaScript for PDF popup functionality and event handling
    - Implemented click right and F12 key prevention with user notifications
    - Initial project version 1.1.0

---
+++ script.js
@@ -0,0 +1,63 @@
+function openPopup(pdfFile) {
+    var popup = document.getElementById('popup');
+    var embed = popup.querySelector('embed');
+    embed.setAttribute('src', pdfFile);
+    popup.style.display = 'flex';
+}
+
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
+
+
+// configuraciones basicas de privacidad
+
+// Deshabilitar el clic derecho del mouse
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
+
+// Deshabilitar la tecla F12
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+});
+
+
+// Lanza un mensaje de web_push de alert
+
+function showNotification(message) {
+    const notification = document.createElement('div');
+    notification.className = 'notification';
+    notification.textContent = message;
+
+    document.body.appendChild(notification);
+
+    // Forzar un reflow para que la transición de opacidad se aplique
+    requestAnimationFrame(() => {
+        notification.classList.add('show');
+    });
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+        notification.addEventListener('transitionend', () => {
+            notification.remove();
+        }, { once: true });
+    }, 2000); // El mensaje desaparecerá después de 2 segundos
+}
+
+// Deshabilitar el clic derecho del mouse y mostrar un mensaje
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    showNotification('Acción no permitida');
+});
+
+// Deshabilitar la tecla F12 y mostrar un mensaje
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+        showNotification('Acción no permitida');
+    }
+});
